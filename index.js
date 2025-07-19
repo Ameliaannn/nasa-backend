88096f3a6018d7294081aecc449bc40835d6db45
An <anru@tcd.ie>
2025-07-19 14:20:00 +0100

    add top10

--- index.js
+++ index.js
@@ -2,26 +2,28 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-// 中间件
+
 app.use(cors());
 app.use(express.json());
 
-// 路由导入
+
 const nasaRouter = require('./routes/nasaRouter');
 const marsRouter = require('./routes/marsRouter');
 const epicRouter = require('./routes/epicRouter');
 const asteroidRouter = require('./routes/asteroidRouter');
+const top10Route = require('./routes/top10');
 
-// 路由挂载（避免重复）
+
 app.use('/api/nasa', nasaRouter);
 app.use('/api/mars', marsRouter);
 app.use('/api/epic', epicRouter);
-app.use('/api/asteroids', asteroidRouter); // 让前端直接访问 /api/asteroids?date=xxx
 
-// 启动服务
+app.use('/api/asteroids', asteroidRouter);
+app.use('/api/asteroids/top10', top10Route);
+
 app.listen(5000, () => {
     console.log('🚀 Server running at http://localhost:5000');
 });
 
-// 定时任务
+
 require('./cronjob');
